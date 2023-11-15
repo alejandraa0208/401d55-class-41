f5cc09148e4ec80071cea3920494f69663c4f090
ALEJANDRA ALTAMIRANO <a.altamirano0208@gmail.com>
2023-11-14 21:44:12 -0800

    lab42

--- components/EventForm.js
+++ components/EventForm.js
@@ -1,42 +1,109 @@
-import React, { useState } from 'react';
-import { View, Button, TextInput, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Button, TextInput, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { createCalendarEvent } from '../api/Calendar';
 import { scheduleNotification } from '../api/Notifications';
+import { useTheme } from '../ThemeContext';
 
 export default function EventForm() {
   const [title, setTitle] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const { theme, toggleTheme } = useTheme();
+  const isLightTheme = theme === 'light';
 
-  const handleCreateEvent = () => {
-    // For demonstration, using fixed dates. You'll replace these with user input.
-    const startDate = new Date();
-    const endDate = new Date();
-    endDate.setHours(startDate.getHours() + 1);
+  const handleCreateEvent = async () => {
+    try {
+      createCalendarEvent(title, startDate, endDate);
+      scheduleNotification(title, startDate);
 
-    createCalendarEvent(title, startDate, endDate);
-    scheduleNotification(title, startDate);
+      alert('Event created and reminder set successfully!');
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create event and set reminder');
+    }
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: isLightTheme ? '#FFF' : '#000' }]}>
+      <View style={styles.switchContainer}>
+        <TouchableOpacity onPress={toggleTheme}>
+          <Text style={{ color: isLightTheme ? '#000' : '#FFF' }}>{isLightTheme ? 'Light Mode' : 'Dark Mode'}</Text>
+        </TouchableOpacity>
+      </View>
       <TextInput
-        style={styles.input}
-        placeholder="Event Title"
+        style={[styles.input, { color: isLightTheme ? '#000' : '#FFF', borderColor: isLightTheme ? 'gray' : 'lightgray' }]}
+        placeholder="           Event Title          "
+        placeholderTextColor={isLightTheme ? '#000' : '#FFF'}
         value={title}
         onChangeText={setTitle}
       />
-      <Button title="Create Event" onPress={handleCreateEvent} />
+      <Text style={[styles.label, { color: isLightTheme ? '#000' : '#FFF' }]}>Start Date and Time:</Text>
+      <DateTimePicker
+        value={startDate}
+        mode="datetime"
+        display="default"
+        textColor={isLightTheme ? '#000' : '#FFF'} // This prop can be used on iOS
+        onChange={(event, selectedDate) => setStartDate(selectedDate || startDate)}
+        style={isLightTheme ? styles.datePickerLight : styles.datePickerDark}
+      />
+      <Text style={[styles.label, { color: isLightTheme ? '#000' : '#FFF' }]}>End Date and Time:</Text>
+      <DateTimePicker
+        value={endDate}
+        mode="datetime"
+        display="default"
+        textColor={isLightTheme ? '#000' : '#FFF'}
+        onChange={(event, selectedDate) => setEndDate(selectedDate || endDate)}
+        style={isLightTheme ? styles.datePickerLight : styles.datePickerDark}
+      />
+      <TouchableOpacity 
+        style={[styles.button, { backgroundColor: isLightTheme ? '#000' : '#FFF' }]} 
+        onPress={handleCreateEvent}
+      >
+      <Text style={styles.buttonText}>Remind Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '80%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 90,
   },
   input: {
     borderWidth: 1,
-    borderColor: 'gray',
     padding: 10,
+    marginBottom: 90,
+    width: '100%',
+    borderRadius: 5,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  datePicker: {
+    width: '100%',
     marginBottom: 20,
+  },
+  switchContainer: {
+    position: 'absolute',
+    top: 40,
+    alignItems: 'center',
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#FFF',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 60,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
